de0597dbcd017e2c1a2272d324406aaf5c868dd0
yobome <rustg1n@163.com>
2020-03-20 23:17:56 +0800

    hello echo

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/labstack/echo"
+	"net/http"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "welcome to echo, hello world!")
+	})
+	e.Start(":6060")
+}
